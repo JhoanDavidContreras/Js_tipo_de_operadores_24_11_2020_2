b009bdb56a317829d4368b4cb59a325b493ac174
unknown <Jhoandavidcontreras14@gmail.com>
2020-11-25 18:06:51 -0500

    js_tipo_de_operadore_24_11_2020__2

---
+++ main.js
@@ -0,0 +1,43 @@
+ // operadores de incremento
+//let numero_1=5;
+//let numero_2=15;
+//numero_1++; //incremenar el numero "1";
+//numero_1+=2; // incremenar el numero la cantidad necesario "2";
+//console.log(numero_1);
+
+
+// operadores de decremento
+ // let numero_1=5;
+// let numero_2=15;
+// numero_1 = numero_2;//incremenar el numero "1";
+ //numero_1 -=2; // incremenar el numero la cantidad necesario "2";
+ //console.log(numero_1);
+
+// operadores de Exponenciacion
+//let numero_1=5;
+//let numero_2 =15;
+//numero_1 **=numero_2;
+//console.log(numero_1);
+
+//ooperadores de modulo segun el valor 
+//let numero_1=15;
+//let numero_2=8;
+
+// numero_1 %=numero_2;
+
+//if (numero_1**numero_2) {
+//console.log(numero_1);
+ 
+                                            
+//let numero_1=15;
+//let numero_2=8;
+let resultado;
+
+resultado = nummero_1+nummero_2;
+console.log(resultado);
+resultado = nummero_1-nummero_2;
+console.log(resultado);
+resultado = (nummero_1*nummero_2);
+console.log = (resultado);
+resultado = nummero_1/nummero_2;
+console.log (resultado);
